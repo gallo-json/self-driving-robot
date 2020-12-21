37e936217f0242059a27e5d4f6e66f2ffae9c5ab
gallo-json <gallojsantiago@gmail.com>
2020-12-20 20:27:39 -0600

    boiler

--- src/yolo/main.py
+++ src/yolo/main.py
@@ -1,2 +1,12 @@
 import cv2
+from jetcam.csi_camera import CSICamera
 
+camera = CSICamera(width=224, height=224, capture_width=1080, capture_height=720, capture_fps=30)
+
+while True:
+    img = camera.read()
+
+    cv2.imshow("Image", img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
