8adf8ea7ea1417f6b1aae988a83f74db9b3c4aff
gallo-json <gallojsantiago@gmail.com>
2020-12-21 08:28:36 -0600

    ok?

--- src/yolo/main.py
+++ src/yolo/main.py
@@ -3,8 +3,63 @@ from jetcam.csi_camera import CSICamera
 
 camera = CSICamera(width=224, height=224, capture_width=1080, capture_height=720, capture_fps=30)
 
+net = cv2.dnn.readNet("stop-sign/yolov4-custom-colab_best.weights", "stop-sign/conf/yolov4-custom-colab.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+classes = ["stop_sign"]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+font = cv2.FONT_HERSHEY_PLAIN
+starting_time = time.time()
+frame_id = 0
+
 while True:
     img = camera.read()
+
+    height, width, channels = frame.shape
+
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.6)
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            conf = confidences[i]
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, label + " " + str(round(conf, 2)), (x, y + 30), font, 3, color, 3)
+
+    cv2.putText(frame, "FPS: " + str(cap.get(cv2.CAP_PROP_FPS)), (10, 50), font, 2, (0, 0, 0), 1)
 
     cv2.imshow("Image", img)
     
