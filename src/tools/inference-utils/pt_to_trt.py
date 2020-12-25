96a395bc439c5d2f45d1cd7b87206c8447ba7495
gallo-json <gallojsantaigo@gmail.com>
2020-12-25 12:26:43 -0800

    got rid of symlinks

--- src/tools/inference-utils/pt_to_trt.py
+++ src/tools/inference-utils/pt_to_trt.py
@@ -1,1 +1,12 @@
-/home/jetbot/yolov3-archive/pt_to_trt.py
+from models import *
+from torch2trt import torch2trt
+import torch
+
+model = Darknet('/home/jetbot/self-driving-robot/src/yolo/stop-sign/conf/yolov4-custom-for-torch.cfg').eval().cuda()
+model.load_state_dict(torch.load('/home/jetbot/self-driving-robot/src/yolo/stop-sign/yolov4-custom-for-torch_last.pt')['model'])
+
+x = torch.ones((1, 3, 416, 416)).cuda()
+
+model_trt = torch2trt(model, [x])
+
+torch.save(model_trt.state_dict(), 'yolov4.pth')
