1e33b9e4582e333a0c120ec1e08de85045c94bfc
gallo-json <gallojsantiago@gmail.com>
2020-12-25 15:50:15 -0600

    comments

--- src/tools/inference-utils/pt_to_trt.py
+++ src/tools/inference-utils/pt_to_trt.py
@@ -1,12 +1,16 @@
+# This file references code from the repo https://github.com/ultralytics/yolov3/tree/archive but it is not part of it
+# Run this file inside that cloned repo to get no errors
+# torch2trt was made primarily for the Jetson Nano, run it on that.
+
 from models import *
 from torch2trt import torch2trt
 import torch
 
 model = Darknet('/home/jetbot/self-driving-robot/src/yolo/stop-sign/conf/yolov4-custom-for-torch.cfg').eval().cuda()
-model.load_state_dict(torch.load('/home/jetbot/self-driving-robot/src/yolo/stop-sign/yolov4-custom-for-torch_last.pt')['model'])
+model.load_state_dict(torch.load('/home/jetbot/self-driving-robot/src/yolo/stop-sign/weights/yolov4-custom-for-torch_best.pt')['model'])
 
 x = torch.ones((1, 3, 416, 416)).cuda()
 
 model_trt = torch2trt(model, [x])
 
-torch.save(model_trt.state_dict(), 'yolov4.pth')
+torch.save(model_trt.state_dict(), 'yolov4.trt')
--- src/tools/inference-utils/yolo_to_onnx.py
+++ src/tools/inference-utils/yolo_to_onnx.py
@@ -1,52 +1,29 @@
-# yolo_to_onnx.py
+# https://github.com/jkjung-avt/tensorrt_demos
+# This file is in the above repository and references code of it
+# Run this file in the cloned repository above to get no errors
+# Run this on Nano with the rest of the rep files
+
+#MIT License
 #
-# Copyright 1993-2019 NVIDIA Corporation.  All rights reserved.
+#Copyright (c) 2019 JK Jung
 #
-# NOTICE TO LICENSEE:
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
 #
-# This source code and/or documentation ("Licensed Deliverables") are
-# subject to NVIDIA intellectual property rights under U.S. and
-# international Copyright laws.
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
 #
-# These Licensed Deliverables contained herein is PROPRIETARY and
-# CONFIDENTIAL to NVIDIA and is being provided under the terms and
-# conditions of a form of NVIDIA software license agreement by and
-# between NVIDIA and Licensee ("License Agreement") or electronically
-# accepted by Licensee.  Notwithstanding any terms or conditions to
-# the contrary in the License Agreement, reproduction or disclosure
-# of the Licensed Deliverables to any third party without the express
-# written consent of NVIDIA is prohibited.
-#
-# NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
-# LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
-# SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
-# PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
-# NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
-# DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
-# NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
-# NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
-# LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
-# SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
-# DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
-# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
-# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
-# OF THESE LICENSED DELIVERABLES.
-#
-# U.S. Government End Users.  These Licensed Deliverables are a
-# "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
-# 1995), consisting of "commercial computer software" and "commercial
-# computer software documentation" as such terms are used in 48
-# C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
-# only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
-# 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
-# U.S. Government End Users acquire the Licensed Deliverables with
-# only those rights set forth herein.
-#
-# Any use of the Licensed Deliverables in individual and commercial
-# software must include, in the user documentation and internal
-# comments to the code, the above Disclaimer and U.S. Government End
-# Users Notice.
-#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 
 
 from __future__ import print_function
