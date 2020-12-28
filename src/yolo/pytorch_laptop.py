476275c1b2c635719fcea8967a9c5ced6be415b5
gallo-json <gallojsantiago@gmail.com>
2020-12-28 09:48:19 -0600

    works

--- src/yolo/pytorch_laptop.py
+++ src/yolo/pytorch_laptop.py
@@ -1,57 +1,119 @@
 import sys
+import argparse
 sys.path.insert(1, '/home/jose/Programming/aiml/tools/yolov3-archive')
 
 from models import Darknet
 from utils.datasets import *
-from utils.utils import torch_utils, load_classes
+from utils.utils import *
 
-infer_image_size = 224
-cfg = '/home/jose/Programming/aiml/Projects/self-driving-robot/src/yolo/stop-sign/conf/yolov4-custom-for-torch.cfg'
-weights = '/home/jose/Programming/aiml/Projects/self-driving-robot/src/yolo/stop-sign/weights/yolov4-custom-for-torch_best.pt'
-names = '/home/jose/Programming/aiml/Projects/self-driving-robot/src/yolo/stop-sign/conf/obj.names'
-confidence_thres = 0.3
-iou_thres = 0.6
+def detect(save_img=False):
+    imgsz = opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
+    out, source, weights, half, view_img, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt
+    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
 
-# Select CUDA device
-device = torch_utils.select_device()
+    # Initialize
+    device = torch_utils.select_device(opt.device)
 
-# Load the YOLO model
-model = Darknet(cfg)
-model.load_state_dict(torch.load(weights, map_location=device)['model'])
-model.to(device).eval()
+    # Initialize model
+    model = Darknet(opt.cfg, imgsz)
 
-# Load the class names
-names = load_classes(names)
+    # Load weights
+    model.load_state_dict(torch.load(weights, map_location=device)['model'])
 
-# Choose random colors for bounding box
-colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+    # Eval mode
+    model.to(device).eval()
 
-### Start INFFERENCE
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+    # Half precision
+    half = half and device.type != 'cpu'  # half precision only supported on CUDA
+    if half: model.half()
 
-t0 = time.time()
+    # Set Dataloader
+    view_img = True
+    torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
+    dataset = LoadStreams(source, img_size=imgsz)
 
-# Initialize dummy image
-img = torch.zeros((1, 3, infer_image_size, infer_image_size), device=device)
-_ = model(img.float())
+    # Get names and colors
+    names = load_classes(opt.names)
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
 
-while True:
-    ok, img = cap.read()
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    _ = model(img.half() if half else img.float()) if device.type != 'cpu' else None  # run once
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
 
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
+        if img.ndimension() == 3: img = img.unsqueeze(0)
 
-    img = torch.from_numpy(img).to(device)
-    img = img.float()
-    img /= 255.0
+        # Inference
+        t1 = torch_utils.time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+        t2 = torch_utils.time_synchronized()
 
-    if img.ndimension() == 3: img = img.unsqueeze(0)
+        # to float
+        if half: pred = pred.float()
 
-    # Actual inference
-    t1 = torch_utils.time_synchronized()
-    pred = model(img, augment=False)[0]
-    t2 = torch_utils.time_synchronized()
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
+                                   multi_label=False, classes=opt.classes, agnostic=opt.agnostic_nms)
 
-    pred = non_max_suppression(pred, confidence_thres, iou_thres, multi_label=False, agnostic=False)
+        # Process detections
+        for i, det in enumerate(pred):  # detections for image i
+            p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+
+            #save_path = str(Path(out) / Path(p).name)
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
+            if det is not None and len(det):
+                # Rescale boxes from imgsz to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_img or view_img:  # Add bbox to image
+                        label = '%s %.2f' % (names[int(cls)], conf)
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
+
+            # Print time (inference + NMS)
+            print('%sDone. (%.3fs)' % (s, t2 - t1))
+
+            # Stream results
+            if view_img:
+                cv2.imshow(p, im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='*.cfg path')
+    parser.add_argument('--names', type=str, default='data/coco.names', help='*.names path')
+    parser.add_argument('--weights', type=str, default='weights/yolov3-spp-ultralytics.pt', help='weights path')
+    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
+    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    opt = parser.parse_args()
+    opt.cfg = check_file(opt.cfg)  # check file
+    opt.names = check_file(opt.names)  # check file
+    print(opt)
+
+    with torch.no_grad():
+        detect()
