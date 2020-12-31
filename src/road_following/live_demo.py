cb5fbb8253672ddd933f572469d167166d347a77
gallo-json <gallojsantiago@gmail.com>
2020-12-30 20:14:21 -0600

    no more camera recall :vomiting_emoji:

--- src/road_following/live_demo.py
+++ src/road_following/live_demo.py
@@ -3,6 +3,29 @@ import numpy as np
 import time
 from jetcam.csi_camera import CSICamera
 
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import PIL.Image
+
+model = torchvision.models.resnet18(pretrained=False)
+model.fc = torch.nn.Linear(512, 2)
+model.load_state_dict(torch.load('best_steering_model_xy.pth'))
+
+device = torch.device('cuda')
+model = model.to(device)
+model = model.eval().half()
+
+mean = torch.Tensor([0.485, 0.456, 0.406]).cuda().half()
+std = torch.Tensor([0.229, 0.224, 0.225]).cuda().half()
+
+def preprocess(image):
+    image = PIL.Image.fromarray(image)
+    image = transforms.functional.to_tensor(image).to(device).half()
+    image.sub_(mean[:, None, None]).div_(std[:, None, None])
+    return image[None, ...]
+
 camera = CSICamera(width=224, height=224)
 
 font = cv2.FONT_HERSHEY_PLAIN
