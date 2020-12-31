245f05b0e45a6f8b7ddfa2377fefe935486c6834
gallo-json <gallojsantiago@gmail.com>
2020-12-31 14:11:22 -0600

    ok

--- src/yolo/pytorch_nano.py
+++ src/yolo/pytorch_nano.py
@@ -1,16 +1,18 @@
 import sys
 import argparse
-sys.path.insert(1, '/home/jetbot/yolov3-archive')
+repo_path = '/home/jose/Programming/aiml/tools/yolov3-archive'
+# repo_path = '/home/jetbot/yolov3-archive'
+sys.path.insert(1, repo_path)
 
 from models import Darknet
 from utils.datasets import *
 from utils.utils import *
 
+stop_sign_area_thres = 0
+
 def detect(save_img=False):
     imgsz = opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
-    out, source, weights, half, view_img, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt
-    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
-
+    out, source, weights, half, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.save_txt
     # Initialize
     device = torch_utils.select_device(opt.device)
 
@@ -27,8 +29,6 @@ def detect(save_img=False):
     half = half and device.type != 'cpu'  # half precision only supported on CUDA
     if half: model.half()
 
-    # Set Dataloader
-    view_img = True
     torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
     dataset = LoadStreams(source, img_size=imgsz)
 
@@ -63,7 +63,6 @@ def detect(save_img=False):
         for i, det in enumerate(pred):  # detections for image i
             p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
 
-            #save_path = str(Path(out) / Path(p).name)
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
             if det is not None and len(det):
@@ -77,18 +76,19 @@ def detect(save_img=False):
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    if save_img or view_img:  # Add bbox to image
-                        label = '%s %.2f' % (names[int(cls)], conf)
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
+                    area = (int(xyxy[2]) - int(xyxy[0])) * (int(xyxy[3]) - int(xyxy[1]))
+                    s += 'Area %s px ' % (area)
+
+                    label = '%s %.2f' % (names[int(cls)], conf)
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
 
             # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))
 
             # Stream results
-            if view_img:
-                cv2.imshow(p, im0)
-                if cv2.waitKey(1) == ord('q'):  # q to quit
-                    raise StopIteration
+            cv2.imshow(p, im0)
+            if cv2.waitKey(1) == ord('q'):  # q to quit
+                raise StopIteration
 
     print('Done. (%.3fs)' % (time.time() - t0))
 
@@ -105,7 +105,6 @@ if __name__ == '__main__':
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
     parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
