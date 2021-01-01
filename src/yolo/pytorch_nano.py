d31850f9efd77ae474096e87bcb6177260fc358e
gallo-json <gallojsantiago@gmail.com>
2020-12-31 20:04:58 -0600

    removed useless arguments

--- src/yolo/pytorch_nano.py
+++ src/yolo/pytorch_nano.py
@@ -57,7 +57,7 @@ stop_sign_area_thres = 0
 
 def detect(save_img=False):
     imgsz = opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
-    out, source, weights, half, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.save_txt
+    source, weights, half = opt.source, opt.weights, opt.half 
     # Initialize
     device = torch_utils.select_device(opt.device)
 
@@ -102,7 +102,7 @@ def detect(save_img=False):
 
         # Apply NMS
         pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
-                                   multi_label=False, classes=opt.classes, agnostic=opt.agnostic_nms)
+                                   multi_label=False, classes=None, agnostic=False)
 
         # Process detections
         for i, det in enumerate(pred):  # detections for image i
@@ -133,8 +133,8 @@ def detect(save_img=False):
             x, y = find_optimal_path(im0)
             print(int(x), int(y))
             cv2.circle(im0, (int(x), int(y)), 8, (0, 255, 0), 3)
-            # Stream results
-            cv2.imshow(p,im0)
+            cv2.imshow(p, im0)
+
             if cv2.waitKey(1) == ord('q'):  # q to quit
                 raise StopIteration
 
@@ -146,16 +146,11 @@ if __name__ == '__main__':
     parser.add_argument('--names', type=str, default='data/coco.names', help='*.names path')
     parser.add_argument('--weights', type=str, default='weights/yolov3-spp-ultralytics.pt', help='weights path')
     parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
     parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     opt = parser.parse_args()
     opt.cfg = check_file(opt.cfg)  # check file
